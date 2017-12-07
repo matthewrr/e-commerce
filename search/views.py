9dc0a45fda99d6c26b9a579d794215f5cdf15228
mrrivera <matthewrr@users.noreply.github.com>
2017-12-07 21:07:00 +0000

    Create standalone search page and add user query text to search results

--- search/views.py
+++ search/views.py
@@ -6,7 +6,7 @@ from django.views.generic import ListView
 from products.models import Product
 
 class SearchProductView(ListView):
-    template_name = "products/list.html"
+    template_name = "search/view.html"
     
     def get_queryset(self, *args, **kwargs):
         request = self.request
