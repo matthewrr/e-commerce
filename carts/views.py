813d96432a217cad8606654c08ee0b119928932f
mrrivera <matthewrr@users.noreply.github.com>
2017-12-09 19:42:06 +0000

    Add cart model and view

--- carts/views.py
+++ carts/views.py
@@ -4,4 +4,10 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 def cart_home(request):
+    cart_id = request.session.get("card_id", None)
+    if cart_id is None: #and isinstance(cart_id, int):
+        print("Create new cart")
+        request.session['card_id'] = 12
+    else:
+        print("Cart ID exists")
     return render(request, "carts/home.html", {})
