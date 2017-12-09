2e924e5476d1ea7e704fbe5377ac0c4fa22b4240
mrrivera <matthewrr@users.noreply.github.com>
2017-12-09 20:16:00 +0000

    Expand cart funtionality in views

--- carts/views.py
+++ carts/views.py
@@ -3,11 +3,25 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
+from .models import Cart
+
+def cart_create(user=None):
+    cart_obj = Cart.objects.create(user=None)
+    print("New cart created")
+    return cart_obj
+
 def cart_home(request):
-    cart_id = request.session.get("card_id", None)
-    if cart_id is None: #and isinstance(cart_id, int):
-        print("Create new cart")
-        request.session['card_id'] = 12
-    else:
+    request.session['cart_id'] = "12"
+    cart_id = request.session.get('card_id', None)
+    # if cart_id is None:
+    #     cart_obj = cart_create()
+    #     request.session['card_id'] = cart_obj.id
+    # else:
+    qs = Cart.objects.filter(id=cart_id)
+    if qs.count() == 1:
         print("Cart ID exists")
+        cart_obj = qs.first()
+    else:
+        cart_obj = cart_create()
+        request.session['card_id'] = cart_obj.id
     return render(request, "carts/home.html", {})
