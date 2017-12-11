b2417123aac57512f6a20282d7ec6d8072e877b6
mrrivera <matthewrr@users.noreply.github.com>
2017-12-11 17:46:47 +0000

    Add cart logic for price total/subtotal

--- carts/models.py
+++ carts/models.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 
 from django.conf import settings
 from django.db import models
+from django.db.models.signals import pre_save, post_save, m2m_changed
 
 from products.models import Product
 
@@ -11,7 +12,7 @@ User = settings.AUTH_USER_MODEL
 class CartManager(models.Manager):
 
     def new_or_get(self, request):
-        cart_id = request.session.get('card_id', None)
+        cart_id = request.session.get("cart_id", None)
         qs = self.get_queryset().filter(id=cart_id)
         if qs.count() == 1:
             new_obj = False
@@ -22,7 +23,7 @@ class CartManager(models.Manager):
         else:
             cart_obj = Cart.objects.new(user=request.user)
             new_obj = True
-            request.session['card_id'] = cart_obj.id
+            request.session['cart_id'] = cart_obj.id
         return cart_obj, new_obj
     
     def new(self, user=None):
@@ -35,6 +36,7 @@ class CartManager(models.Manager):
 class Cart(models.Model):
     user = models.ForeignKey(User, null=True, blank=True) #carts for auth & non-authenticated
     products = models.ManyToManyField(Product, blank=True)
+    subtotal = models.DecimalField(default=0.00, max_digits=100, decimal_places=2)
     total = models.DecimalField(default=0.00, max_digits=100, decimal_places=2)
     updated = models.DateTimeField(auto_now=True)
     timestamp = models.DateTimeField(auto_now_add=True)
@@ -43,3 +45,20 @@ class Cart(models.Model):
     
     def __str__(self):
         return str(self.id)
+        
+def m2m_changed_cart_receiver(sender, instance, action, *args, **kwargs):
+    if action =='post_add' or action == 'post_remove' or action == 'post_clear':
+        products = instance.products.all()
+        total = 0
+        for x in products:
+            total += x.price
+        if instance.subtotal != total:
+            instance.subtotal = total
+            instance.save()
+
+m2m_changed.connect(m2m_changed_cart_receiver, sender=Cart.products.through)
+
+def pre_save_cart_receiver(sender, instance, *args, **kwargs):
+    instance.total = instance.subtotal + 1
+
+pre_save.connect(pre_save_cart_receiver, sender=Cart)
